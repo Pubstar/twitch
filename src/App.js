01a0158d3432557929209cd30c243ca5d30188dd
Pieter Hoek <pieterhoek@msn.com>
2024-09-13 22:41:40 +0200

    Add missing channel name

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <div>
         <h1>Live channels you might like</h1>
         <div className='rows-container'>
-          <Row snapshot={snapshot_Fextralife} viewers="42.1k" name="" profile_img={profile_Fextralife} title="!MOUNT 🔴NEW !DROPS🔴 ~ NECROMANCER WITH CAS" game="Diablo IV" tags={["DropsEnabled", "English"]} />
+          <Row snapshot={snapshot_Fextralife} viewers="42.1k" name="Fextralife" profile_img={profile_Fextralife} title="!MOUNT 🔴NEW !DROPS🔴 ~ NECROMANCER WITH CAS" game="Diablo IV" tags={["DropsEnabled", "English"]} />
           <Row snapshot={snapshot_hasanAbi} viewers="33.3k" name="HasanAbi" profile_img={profile_hasanAbi} title=">H3 VS PEARLYTHING -- TRUMP ARRAIGNMENT HOGWATCH!!!! FD SIGNIFIER JOINS ME LATER!!!" game="Just Chatting" tags={["News", "Politics", "ADHD", "English"]} />
           <Row snapshot={snapshot_theBausffs} viewers="9.2k" name="theBausFfs" profile_img={profile_theBausffs} title="Late night cozyness" game="League of Legends" tags={["Talkative", "English"]} />
           <Row snapshot={snapshot_Towelliee} viewers="3.4k" name="Towelliee" profile_img={profile_Towelliee} title="⚔️NEW !DROPS SOCERER🎁2 GIFTED SUBS = D4 MOUNT🎁SUBATHON DAY 11⚔️!4080 !4090 GIVEAWAY⚔️!CORSAIR !ADV !GLASSES !ORIGIN⚔️" game="Diablo IV" tags={["MMORPG", "Tank", "Cuban", "Boomer"]} />
